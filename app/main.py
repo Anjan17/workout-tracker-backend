c78409ad88cb64407df44e80c8eca61011e82388
Anjan <anjan.behera123@gmail.com>
2026-06-21 23:53:59 +0530

    Add exercise router and database session

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from fastapi.middleware import Middleware
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.api.exercises import router as exercises_router
+
 middleware = [
     Middleware(
         CORSMiddleware,
@@ -20,36 +22,4 @@ async def root():
     return {"message": "Welcome to Hitup app !"}
 
 
-@app.get("/exercises")
-@app.get("/exercises/list")
-async def get_exercises(offset: int = 0, limit: int = 100):
-    return {
-        "get exercises": {
-            "offset": offset,
-            "limit": limit,
-        },
-    }
-
-
-@app.get("/exercises/{exercise_id}")
-async def get_exercise_details(exercise_id: str):
-    return {
-        "get exercise": {
-            "exercise_id": exercise_id,
-        }
-    }
-
-
-@app.post("/exercises/{exercise_id}")
-async def create_exercise(exercise_id: str):
-    pass
-
-
-@app.put("/exercises/{exercise_id}")
-async def update_exercise(exercise_id: str):
-    pass
-
-
-@app.delete("/exercises/{exercise_id}")
-async def delete_exercise(exercise_id: str):
-    pass
+app.include_router(exercises_router)
